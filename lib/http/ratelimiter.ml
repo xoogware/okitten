2567edc5854e404a76e3d33a73e634a33f7c963b
Andrew Brower <bigspeedfpv@gmail.com>
2024-06-30 22:35:51 +0200

    feat: parse headers on response

--- lib/http/ratelimiter.ml
+++ lib/http/ratelimiter.ml
@@ -1,12 +1,63 @@
 open Lwt
 module RouteMap = Map.Make (String)
 
-type ratelimit =
-  { limit : int
-  ; remaining : int
-  ; reset_at : float
-  ; reset_after : float
-  }
+module Ratelimit = struct
+  type t =
+    { limit : int
+    ; remaining : int
+    ; reset_at : float option
+    ; reset_after : float option
+    }
+
+  let preprocess_hook ~resource rl =
+    match rl.limit with
+    | 0 -> return rl
+    | limit ->
+      (match rl.reset_at with
+       | None -> return { rl with remaining = limit }
+       | Some reset_at ->
+         (match reset_at -. Unix.time () with
+          | delay when delay <= 0. ->
+            return
+            @@ if rl.remaining = 0 then rl else { rl with remaining = rl.remaining - 1 }
+          | delay ->
+            (match rl.remaining with
+             | 0 ->
+               Logs.debug (fun m ->
+                 m
+                   "Ratelimit reached on %s, preemptively blocking for %fms."
+                   resource
+                   delay);
+               let%lwt _ = Lwt_unix.sleep (delay /. 1000.) in
+               return rl
+             | _ -> return { rl with remaining = rl.remaining - 1 })))
+  ;;
+
+  let postprocess_hook ~headers rl =
+    let open Cohttp.Header in
+    let limit =
+      match get headers "X-RateLimit-Limit" with
+      | Some l -> int_of_string l
+      | None -> rl.limit
+    in
+    let remaining =
+      match get headers "X-RateLimit-Remaining" with
+      | Some r -> int_of_string r
+      | None -> rl.remaining
+    in
+    let reset_at =
+      match get headers "X-RateLimit-Reset" with
+      | Some r -> Some (float_of_string r)
+      | None -> rl.reset_at
+    in
+    let reset_after =
+      match get headers "X-RateLimit-Reset-After" with
+      | Some r -> Some (float_of_string r)
+      | None -> rl.reset_after
+    in
+    { limit; remaining; reset_at; reset_after }
+  ;;
+end
 
 type ratelimit_info =
   { limit : int
@@ -20,7 +71,7 @@ type request = Cohttp.Code.meth * string * Cohttp.Header.t * Cohttp_lwt.Body.t o
 type request_queue_item = request * (Cohttp_lwt.Response.t * Cohttp_lwt.Body.t) Lwt_mvar.t
 
 type t =
-  { routes : ratelimit RouteMap.t
+  { routes : Ratelimit.t RouteMap.t
   ; request_queue : request_queue_item Lwt_stream.t
   ; push : request_queue_item option -> unit
   ; token : string
@@ -42,50 +93,41 @@ let init token =
 let set_callback self callback = { self with callback }
 let use_absolute_ratelimits self r = { self with use_absolute_ratelimits = r }
 
+let get_or_default route ratelimits =
+  match RouteMap.find_opt route ratelimits with
+  | Some r -> r
+  | None -> Ratelimit.{ limit = 0; remaining = 0; reset_at = None; reset_after = None }
+;;
+
+let apply_preprocess ~resource rl =
+  let%lwt route =
+    rl.routes |> get_or_default resource |> Ratelimit.preprocess_hook ~resource
+  in
+  return { rl with routes = RouteMap.update resource (fun _ -> Some route) rl.routes }
+;;
+
+let apply_postprocess ~headers ~resource rl =
+  let route =
+    rl.routes |> get_or_default resource |> Ratelimit.postprocess_hook ~headers
+  in
+  { rl with routes = RouteMap.update resource (fun _ -> Some route) rl.routes }
+;;
+
 let rec watch_requests ratelimiter =
-  let get_or_default route ratelimits =
-    match RouteMap.find_opt route ratelimits with
-    | Some r -> r
-    | None -> { limit = 0; remaining = 0; reset_at = 0.; reset_after = 0. }
-  in
-  let increment_ratelimit resource (response : Cohttp_lwt.Response.t) self =
-    let limit = Cohttp.Header.get response.headers "X-RateLimit-Limit" in
-    let remaining = Cohttp.Header.get response.headers "X-RateLimit-Remaining" in
-    let reset = Cohttp.Header.get response.headers "X-RateLimit-Reset" in
-    let reset_after = Cohttp.Header.get response.headers "X-RateLimit-Reset-After" in
-    let bucket = Cohttp.Header.get response.headers "X-RateLimit-Bucket" in
-    let updater = function
-      | None -> Some { limit = 0; remaining = 0; reset_at = 0.; reset_after = 0. }
-      | Some _ -> Some { limit = 0; remaining = 0; reset_at = 0.; reset_after = 0. }
-    in
-    let routes = RouteMap.update resource updater self.routes in
-    { self with routes }
-  in
+  let open Ratelimit in
   let perform ~meth ~resource ~headers ~body self =
     let uri = Uri.of_string resource in
     let%lwt response, body = Cohttp_lwt_unix.Client.call ~headers ?body meth uri in
-    let self = increment_ratelimit resource response self in
     return (response, body, self)
-  in
-  let ratelimit_precheck now route =
-    return
-    @@ if route.reset_at < now then { route with remaining = route.limit } else route
   in
   match%lwt Lwt_stream.get ratelimiter.request_queue with
   | Some (request, response_channel) ->
-    let now = Unix.time () in
     let meth, resource, headers, body = request in
-    let%lwt route =
-      get_or_default resource ratelimiter.routes |> ratelimit_precheck now
-    in
-    let ratelimiter =
-      { ratelimiter with
-        routes = RouteMap.update resource (fun _ -> Some route) ratelimiter.routes
-      }
-    in
+    let%lwt ratelimiter = apply_preprocess ~resource ratelimiter in
     let%lwt response, body, ratelimiter =
       perform ~meth ~resource ~headers ~body ratelimiter
     in
+    let ratelimiter = apply_postprocess ~headers ~resource ratelimiter in
     let%lwt _ = Lwt_mvar.put response_channel (response, body) in
     watch_requests ratelimiter
   | None ->
